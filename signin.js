92e1e31864c7040cd419be2f6db2abaa92d11725
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-07 23:51:09 +0300

    create a signin container

--- signin.js
+++ signin.js
@@ -8,3 +8,23 @@ let signup = (() => {
         }
         return variable;;
     }
+     //change tempDiv to body once out of VSCode initialized template
+     let signInContainer = createElement('signInContainer');
+     const tempDiv = document.querySelector('body');
+     tempDiv.appendChild(signInContainer);
+ 
+     let companyNameContainer = createElement('companyNameContainer');
+     let companyName = document.createElement('img')
+     companyName.setAttribute('id', 'companyName')
+     companyName.src = "images/logo_transparent.png"
+     companyNameContainer.appendChild(companyName)
+ 
+     let welcomeContainer = createElement('welcomeContainer');
+     let welcomeText = createElement('welcomeText');
+     welcomeText.innerHTML = "Welcome Back!"
+     welcomeContainer.appendChild(welcomeText)
+ 
+     let signInButton = createElement('signInButton', "button");
+     let signInButtonText = createElement('signInButtonText');
+     signInButtonText.innerHTML = "Sign In"
+     signInButton.appendChild(signInButtonText)
