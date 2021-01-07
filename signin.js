f0d385bec84925736971b11e3f427d72a5e4e944
Dennis Mwangi <dennistrevor06@gmail.com>
2021-01-07 23:51:43 +0300

    password input Area

--- signin.js
+++ signin.js
@@ -28,3 +28,26 @@ let signup = (() => {
      let signInButtonText = createElement('signInButtonText');
      signInButtonText.innerHTML = "Sign In"
      signInButton.appendChild(signInButtonText)
+
+     let emailPasswordContainer = createElement('emailPasswordContainer');
+     let emailContainer = createElement('emailContainer')
+     let emailInputArea = createElement('emailInputArea', "", 'textarea');
+     emailInputArea.setAttribute('name', 'email')
+     emailInputArea.setAttribute('scroll', 'none')
+     emailInputArea.setAttribute('placeholder', 'Email Address')
+     let emailIcon = createElement('emailIcon', "icon", "img")
+     emailIcon.src = "images/envelope.svg"
+     emailContainer.appendChild(emailIcon)
+     emailContainer.appendChild(emailInputArea)
+     emailPasswordContainer.appendChild(emailContainer)
+ 
+     let passwordInputArea = createElement('passwordInputArea', "", 'input');
+     let passwordContainer = createElement('passwordContainer')
+     passwordInputArea.setAttribute('name', 'password')
+     passwordInputArea.setAttribute('placeholder', "Password")
+     passwordInputArea.setAttribute('type', "password")
+     let passwordIcon = createElement('passwordIcon', "icon", "img")
+     passwordIcon.src = "images/padlock.svg"
+     passwordContainer.appendChild(passwordIcon)
+     passwordContainer.appendChild(passwordInputArea)
+     emailPasswordContainer.appendChild(passwordContainer)
